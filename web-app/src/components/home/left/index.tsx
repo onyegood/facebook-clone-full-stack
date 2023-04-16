dc0ff98b9e86247e1a7f76f0caf84b79909f3260
onyegood <goodmomen@gmail.com>
2023-04-16 08:54:52 +0100

    feat: added shortcut list

--- web-app/src/components/home/left/index.tsx
+++ web-app/src/components/home/left/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import { UserInfoType } from '../../../types/user';
 import LeftLink from './LeftLink';
 import { left } from '../../../data/home';
 import { Link } from 'react-router-dom';
+import { ArrowDown1 } from '../../../svg';
+import ShortcutItem from './ShortcutItem';
 
 type LeftSideBarProps = {
   user: UserInfoType;
 };
 
 const LeftSideBar: React.FC<LeftSideBarProps> = ({ user }) => {
+  const [showAll, setShowAll] = useState(false);
+  const START_FROM = showAll ? 8 : 0;
+  const STOP_AT = showAll ? left.length : 8;
+
   return (
-    <div className="left_sidebar">
+    <div className="left_sidebar scrollbar">
       <Link to="/profile" className="left_link hover1">
         <img src={user?.image} alt={user?.first_name} />
         <span>
@@ -19,9 +25,32 @@ const LeftSideBar: React.FC<LeftSideBarProps> = ({ user }) => {
           {user?.last_name}
         </span>
       </Link>
-      {left.slice(0, 8).map((link, i) => (
+      {left.slice(START_FROM, STOP_AT).map((link, i) => (
         <LeftLink key={i + 1} {...link} />
       ))}
+      <div className="left_link hover1" onClick={() => setShowAll(!showAll)}>
+        <div className={`small_circle ${showAll && 'rotate360'}`}>
+          <ArrowDown1 />
+        </div>
+        <span>See {showAll ? 'less' : 'more'}</span>
+      </div>
+      <div className="splitter"></div>
+      <div className="shortcut">
+        <div className="heading">Your shortcuts</div>
+        <div className="edit_shortcut">Edit</div>
+      </div>
+      <div className="shortcut_list">
+        <ShortcutItem
+          name="My youtube channel"
+          link="http://localhost.com/youtube"
+          img="../../images/ytb.png"
+        />
+        <ShortcutItem
+          name="My instagram"
+          link="http://localhost.com/youtube"
+          img="../../images/insta.png"
+        />
+      </div>
     </div>
   );
 };
