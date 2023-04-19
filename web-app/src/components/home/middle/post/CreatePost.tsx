cc63bff675d1a4de01373aa762549eeee8aaf6bb
onyegood <goodmomen@gmail.com>
2023-04-19 23:49:31 +0100

    feat: added more stylings

--- web-app/src/components/home/middle/post/CreatePost.tsx
+++ web-app/src/components/home/middle/post/CreatePost.tsx
@@ -1,5 +1,6 @@
+import { LiveVideo, Photo } from '../../../../svg';
 import { UserInfoType } from '../../../../types/user';
-import '../style.css';
+import './style.css';
 
 type CreatePostProps = {
   user: UserInfoType;
@@ -15,6 +16,20 @@ const CreatePost: React.FC<CreatePostProps> = ({ user }) => {
         </div>
       </div>
       <div className="create_post_splitter"></div>
+      <div className="create_post_body">
+        <div className="create_post_icon hover1">
+          <LiveVideo color="#f3425f" />
+          Live video
+        </div>
+        <div className="create_post_icon hover1">
+          <Photo color="#4bbf67" />
+          Photo/Video
+        </div>
+        <div className="create_post_icon hover1">
+          <LiveVideo color="#f7b920" />
+          Feeling/Activity
+        </div>
+      </div>
     </div>
   );
 };
